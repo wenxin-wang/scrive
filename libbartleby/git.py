f23459e34eeead539c85bcc0f0f5102d6d3c333f
Charlie Brown <stieizc.33@gmail.com>
2014-02-23 22:43:15 +0800

    Git: add get_toplevel

--- libbartleby/git.py
+++ libbartleby/git.py
@@ -1,3 +1,9 @@
 # A wrapper library for git
 
+from subprocess import call
+
 class Repository:
+    @staticmethod
+    def get_toplevel():
+        """Get the toplevel of a project"""
+        call(["git", "rev-parse", "--show-toplevel"])
