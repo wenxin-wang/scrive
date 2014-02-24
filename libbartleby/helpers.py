76306c07e0f66eda7554508857d877468c576353
Charlie Brown <stieizc.33@gmail.com>
2014-02-24 20:32:41 +0800

    helpers: add dir_empty

--- libbartleby/helpers.py
+++ libbartleby/helpers.py
@@ -1,7 +1,14 @@
-import sys
+import sys, os
 
 configname = 'config.ini' if sys.platform == 'win32' else '.config'
 
 def log(msg):
     """Log msg"""
     print(msg, file=sys.stderr)
+
+def dir_empty(dir):
+    """
+    Check if dir is empty.
+    We do not check if dir is relative.
+    """
+    return not os.listdir(dir)
