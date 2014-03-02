a01b350ef761713f5bc4f80b727ff13cc7dbe1d9
Charlie Brown <stieizc.33@gmail.com>
2014-03-02 19:11:05 +0800

    config: add readcfg

--- libbartleby/config.py
+++ libbartleby/config.py
@@ -7,14 +7,20 @@ class Config(configparser.ConfigParser):
         """instantiate a config parser"""
         self.cfgfile = file
         super().__init__()
-        if os.path.isfile(self.cfgfile):
-            self.read(file)
+        self.readcfg()
+
     def __str__(self):
         return '\n'.join('[' + section + ']\n' + '\n'.join(str(k)+'='+str(v) for (k,v) in self[section].items()) for section in self.sections())
+
+    def readcfg(self):
+        if os.path.isfile(self.cfgfile):
+            self.read(file)
+
     def writecfg(self):
         """Write a config file to disk"""
         with open(self.cfgfile, 'w') as cfg:
             self.write(cfg)
+
     def set_orig_lang(self, lang, force=False):
         """Set the language of original project"""
         if self.has_option("Original", "lang") and not force:
